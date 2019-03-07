55f12f4bd53b5395bf2e29b55bb82a09b8416acc
nileshkumarsinha27 <nileshsinha.sinha0@gmail.com>
2019-03-07 12:58:33 +0530

    Add test cases for saga

--- tests/Articles.saga.test.js
+++ tests/Articles.saga.test.js
@@ -1,27 +1,29 @@
-import sagaHelper from 'redux-saga-testing';
+import assert from 'assert';
 import { put } from 'redux-saga/effects';
 import ACTIONS from '../src/api-config/actions.constants';
-
 import getArticles from '../src/components/articles/Articles.saga';
 
-describe('getArticles saga testing', () => {
-	const it = sagaHelper(getArticles());
-	it('should then trigger an action', result => {
-		if (result.type === 'GET_ARTICLES_LIST_SUCCESS') {
-			expect(result).toEqual(
-				put({
-					type: ACTIONS.ARTICLES.GET_ARTICLES_LIST_SUCCESS,
-					response: result
-				})
-			);
-		}
-		else {
-			expect(result).toEqual(
-				put({
-					type: ACTIONS.ARTICLES.GET_ARTICLES_LIST_FAIL,
-					response: result
-				})
-			);
-		}
+const articleData = {
+	copyright:
+		'Copyright (c) 2019 The New York Times Company. All Rights Reserved.',
+	last_updated: '2019-03-04T03:17:04-05:00',
+	num_results: 1,
+	results: [{ section: 'World', subsection: 'Asia Pacific' }],
+	section: 'home',
+	status: 'OK'
+};
+
+describe('getArticles saga', () => {
+	const articleGenerator = getArticles();
+	beforeEach(() => {
+		articleGenerator.next();
 	});
+	assert.deepEqual(
+		articleGenerator.next(articleData).value,
+		put({
+			type: ACTIONS.ARTICLES.GET_ARTICLES_LIST_SUCCESS,
+			response: articleData
+		}),
+		'it calls GET_ARTICLES_LIST_SUCCESS'
+	);
 });
--- tests/Books.saga.test.js
+++ tests/Books.saga.test.js
@@ -1,27 +1,55 @@
-import sagaHelper from 'redux-saga-testing';
+import assert from 'assert';
 import { put } from 'redux-saga/effects';
 import ACTIONS from '../src/api-config/actions.constants';
-
 import getBooks from '../src/components/books/Books.saga';
 
-describe('getBooks saga testing', () => {
-	const it = sagaHelper(getBooks());
-	it('should then trigger an action', result => {
-		if (result.type === 'GET_BOOK_LIST_SUCCESS') {
-			expect(result).toEqual(
-				put({
-					type: ACTIONS.BOOKS.GET_BOOK_LIST_SUCCESS,
-					result
-				})
-			);
+const bookData = {
+	copyright:
+		'Copyright (c) 2019 The New York Times Company.  All Rights Reserved.',
+	last_modified: '2019-02-27T23:38:02-05:00',
+	num_results: 1,
+	results: [
+		{
+			amazon_product_url:
+				'https://www.amazon.com/Where-Crawdads-Sing-Delia-Owens/dp/0735219095?tag=NYTBS-20',
+			asterisk: 0,
+			bestsellers_date: '2019-02-23',
+			book_details: [{ title: 'WHERE THE CRAWDADS SING' }],
+			dagger: 0,
+			display_name: 'Hardcover Fiction',
+			isbns: [
+				{ isbn10: '0735219095', isbn13: '9780735219090' },
+				{ isbn10: '0735219117', isbn13: '9780735219113' }
+			],
+			list_name: 'Hardcover Fiction',
+			published_date: '2019-03-10',
+			rank: 1,
+			rank_last_week: 1,
+			reviews: [
+				{
+					book_review_link: '',
+					first_chapter_link: '',
+					sunday_review_link: '',
+					article_chapter_link: ''
+				}
+			],
+			weeks_on_list: 25
 		}
-		else {
-			expect(result).toEqual(
-				put({
-					type: ACTIONS.BOOKS.GET_BOOK_LIST_FAIL,
-					result
-				})
-			);
-		}
+	],
+	status: 'OK'
+};
+
+describe('getBooks saga', () => {
+	const bookgenerator = getBooks();
+	beforeEach(() => {
+		bookgenerator.next();
 	});
+	assert.deepEqual(
+		bookgenerator.next(bookData).value,
+		put({
+			type: ACTIONS.BOOKS.GET_BOOK_LIST_SUCCESS,
+			response: bookData
+		}),
+		'it calls GET_BOOK_LIST_SUCCESS'
+	);
 });
